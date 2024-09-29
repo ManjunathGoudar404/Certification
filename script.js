ab014395e69a881dca5975d2b783601abaeb6369
ManjunathGoudar404 <manjunathgoudar404@gmail.com>
2024-09-29 16:33:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
+let btn3 = document.querySelector('#btn3')
+let btn4 = document.querySelector('#btn4')
+let btn5 = document.querySelector('#btn5')
+let btn6 = document.querySelector('#btn6')
+let btn7 = document.querySelector('#btn7')
+let btn8 = document.querySelector('#btn8')
+let btn9 = document.querySelector('#btn9')
+let btn10 = document.querySelector('#btn10')
+let btn11 = document.querySelector('#btn11')
+let btn12 = document.querySelector('#btn12')
+let btn13 = document.querySelector('#btn13')
+let btn14 = document.querySelector('#btn14')
+let btn15 = document.querySelector('#btn15')
+let btn16 = document.querySelector('#btn16')
+let btn17 = document.querySelector('#btn17')
+let btn18 = document.querySelector('#btn18')
+let btn19 = document.querySelector('#btn19')
+let btn20 = document.querySelector('#btn20')
+
+
+
+btn1.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.kaggle.com/learn/certification/manjunathgoudar272/python")
+})
+btn2.addEventListener('click',function(){
+    window.open("https://www.udemy.com/certificate/UC-b4012962-7e37-4e03-92a3-b05db7567657/")
+})
+btn3.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-ca8be9eb-533e-4043-bb13-bad2fbfa03ce/")
+})
+btn4.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-5cdb614f-4ad0-40ea-86ed-daf52b2e14ed/")
+})
+btn5.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-89956f2c-928a-4631-aa3a-0bce06b9ed3e/")
+})
+btn6.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-51dd146b-6c06-45e8-b300-aaeda06a0469/")
+})
+btn7.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://freecodecamp.org/certification/ManjunathGoudar/responsive-web-design")
+})
+btn8.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-f56081eb-dbe5-4091-812e-208398841af3/")
+})
+btn9.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-595dbefc-9e52-4a3c-9d31-437abc1ea07d/")
+})
+btn10.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-f0456eee-48a4-4da4-ad59-7df6187c88b0/")
+})
+btn11.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-20032036-7199-4299-9129-f37ac2eb494c/")
+})
+btn12.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-fe3c360c-fa4d-481e-81bc-ab3a7b5236db/")
+})
+btn13.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-736e28a3-a4ea-4805-bb06-74b76275bf63/")
+})
+btn14.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://archive.nptel.ac.in/noc/Ecertificate/?q=NPTEL23CS108S4315006220174421")
+})
+btn15.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-e516a801-676d-4705-8ed0-538aaeaa3da7/")
+})
+btn16.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.credly.com/users/manjunath-goudar.51fc9f76/edit")
+})
+btn17.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.udemy.com/certificate/UC-14429df9-a7de-421a-9c54-66301b590daf/")
+})
+btn18.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.kaggle.com/learn/certification/manjunathgoudar272/python")
+})
+btn19.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.kaggle.com/learn/certification/manjunathgoudar272/python")
+})
+btn20.addEventListener('click',function(){
+    // location.href="https://www.kaggle.com/learn/certification/manjunathgoudar272/python"
+    window.open("https://www.kaggle.com/learn/certification/manjunathgoudar272/python")
+})
